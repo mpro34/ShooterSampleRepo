aa0fbceb54546c5492b4fcd5c63333705ee93858
Chris Whiting <mpro34@gmail.com>
2020-04-25 12:22:44 -0500

    First commit for unreal shooter sampler

---
+++ Source/ShooterSample/ShooterSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShooterSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERSAMPLE_API AShooterSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
